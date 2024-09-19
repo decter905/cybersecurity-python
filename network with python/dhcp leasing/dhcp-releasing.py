1f9a0ace20fd500e45ec8953898723e0baa4d501
rei905 <dexter.zurbruegg@tie-international.com>
2024-09-19 09:03:19 +0200

    rename

--- network with python/dhcp leasing/dhcp-releasing.py
+++ network with python/dhcp leasing/dhcp-releasing.py
@@ -1,56 +1,25 @@
 from scapy.all import *
+
 from scapy.layers.dhcp import BOOTP, DHCP
-from scapy.layers.inet import UDP, IP
-import random
-
-client_mac = "F3:77:C3:1D:04:03"
-
-my_ip = "10.16.1.94"
-dhcp_server_ip = "10.16.0.1"
-dhcp_server_mac = "d8:58:d7:01:10:cf"
-iface = "Ethernet 4"
-
-fam, hw = get_if_raw_hwaddr(iface)
+from scapy.layers.inet import IP, UDP
 
 
 def mac_to_bytes(mac_addr: str) -> bytes:
     return int(mac_addr.replace(":", ""), 16).to_bytes(6, "big")
 
 
-def dhcp_release():
-    release_packet = (
-            IP(
-                src=my_ip,
-                dst=dhcp_server_ip
-            ) /
-            UDP(
-                sport=68,
-                dport=67
-            ) /
-            BOOTP(
-                chaddr=hw,
-                ciaddr="0.0.0.0",
-                xid=random.randint(1, 0xffffffff)
-            ) /
-            DHCP(options=[
-                ("message-type", "release"),
-                ("server_id", dhcp_server_ip),
-                "end"
-                ]
-            )
-    )
+conf.checkIPaddr = False
+conf.iface = "Ethernet 5"
 
-    send(release_packet, iface=iface, verbose=True)
-    dhcp_release_filter = "udp and port 67 and src port 68"
-    print("Sniffing...")
-    acks = sniff(iface=iface, filter=dhcp_release_filter, count=1, timeout=5)
+localiface = 'Ethernet 5'
+releaseMAC = 'f2:77:c3:1d:04:03'
+releaseIP = '10.16.1.95'
+serverIP = '10.16.0.1'
+releaseMACraw = mac_to_bytes(releaseMAC)
+transactionid = int(0x7579dc58)
 
-    if acks:
-        print("ACK received: ")
-        print(acks[0].show())
-    else:
-        print("No ACK received.")
-
-
-dhcp_release()
-
+dhcp_release = (IP(dst=serverIP) /
+                UDP(sport=68, dport=67) /
+                BOOTP(chaddr=releaseMACraw, ciaddr=releaseIP, xid=transactionid) /
+                DHCP(options=[('message-type', 'release'), 'end']))
+send(dhcp_release)
